a80d011311cc50db01ca48512b181e5586e2e8e5
solid <solidSRV@gmail.com>
2021-01-10 11:23:45 +0100

    Z33 Add initial JUnit test

---
+++ src/test/java/InitialTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InitialTest {
+    @Test
+    void shouldOneEqualsOne() {
+        assertEquals(1, 1);
+    }
+}
